72159761677a8d2e571871cb71e1de13ca6dfdfe
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2021-10-14 21:23:55 +0200

    feat: introduce master nodes and logs configuration

--- varaibles.tf
+++ varaibles.tf
@@ -16,6 +16,36 @@ variable "node_instance_type" {
   default     = "t2.small.elasticsearch"
 }
 
+variable "dedicated_master_type" {
+  description = "Instance type of dedicated master node."
+  default     = "t3.small.elasticsearch"
+}
+
+variable "dedicated_master_count" {
+  description = "Amount of dedicated master nodes."
+  default     = "0"
+}
+
+variable "index_slow_log_group_arn" {
+  description = "XXX"
+  default     = ""
+}
+
+variable "search_slow_log_group_arn" {
+  description = "XXX"
+  default     = ""
+}
+
+variable "es_application_log_group_arn" {
+  description = "XXX"
+  default     = ""
+}
+
+variable "audit_log_group_arn" {
+  description = "XXX"
+  default     = ""
+}
+
 variable "node_disk_size" {
   description = "Disk size of cluster node."
   default     = "10"
@@ -64,17 +94,23 @@ data "aws_subnet" "default" {
 }
 
 locals {
-  project             = var.project
-  vpc_id              = data.aws_subnet.default.vpc_id
-  region              = data.aws_region.current.name
-  account_id          = data.aws_caller_identity.current.account_id
-  domain_name         = var.name
-  domain_arn          = "arn:aws:es:${local.region}:${local.account_id}:domain/${local.domain_name}"
-  version             = var.elasticsearch_version
-  node_instance_type  = var.node_instance_type
-  node_disk_size      = var.node_disk_size
-  node_amount         = var.node_amount
-  subnets             = var.subnets
-  trusted_cidr_blocks = var.trusted_cidr_blocks
-  multi_az            = var.multi_az
+  project                      = var.project
+  vpc_id                       = data.aws_subnet.default.vpc_id
+  region                       = data.aws_region.current.name
+  account_id                   = data.aws_caller_identity.current.account_id
+  domain_name                  = var.name
+  domain_arn                   = "arn:aws:es:${local.region}:${local.account_id}:domain/${local.domain_name}"
+  version                      = var.elasticsearch_version
+  node_instance_type           = var.node_instance_type
+  dedicated_master_count       = var.dedicated_master_count
+  dedicated_master_type        = var.dedicated_master_type
+  index_slow_log_group_arn     = var.index_slow_log_group_arn
+  search_slow_log_group_arn    = var.search_slow_log_group_arn
+  es_application_log_group_arn = var.es_application_log_group_arn
+  audit_log_group_arn          = var.audit_log_group_arn
+  node_disk_size               = var.node_disk_size
+  node_amount                  = var.node_amount
+  subnets                      = var.subnets
+  trusted_cidr_blocks          = var.trusted_cidr_blocks
+  multi_az                     = var.multi_az
 }
